cfe23a2b486c8824b8baa8da43c61042c8d9cb27
TayaTeslya <taya1teslya@gmail.com>
2023-04-09 02:51:05 +0300

    добавление и удаление строк

--- js/level.js
+++ js/level.js
@@ -1,36 +1,41 @@
 // localStorage.setItem('idLevel', '1'); //сохранить - локальное хранилище браузера
 // localStorage.getItem('idLevel'); //получить - короче как сессионные переменные в пхп
 
-console.log(window.location);
+// console.log(window.location);
 
 
 const strsCode = document.getElementById('strs-code');
+const strsCount = document.getElementById('strs-count');
 
 
-strsCode.addEventListener('keydown', (event) => {
-
+strsCode.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        let divCount = document.createElement('div');
+        divCount.textContent = strsCount.children.length + 1;
+        strsCount.appendChild(divCount);
+        // window.getSelection().focusNode.addEventListener('resize', (event) => {
+        //     console.log(event);
+        // });
+        // console.log(window.getSelection().focusNode);
+        // console.log(strsCode.children[0]);
+    }
+    else {
+        let indexEdit;
+        for (let i = 0; i < strsCode.children.length; i++) {
+            if (strsCode.children[i] === window.getSelection().focusNode.parentNode) {
+                indexEdit = i;
+                break;
+            }
+        }
+        if (getComputedStyle(strsCode.children[indexEdit]).height !== getComputedStyle(strsCount.children[indexEdit]).height) {
+            strsCount.children[indexEdit].style.height = getComputedStyle(strsCode.children[indexEdit]).height;
+        }
+    }
+    if (event.key === 'Backspace') {
+        if (strsCode.children.length === 1 && strsCode.children[0].textContent.length <= 0) {
+            event.preventDefault(); // чтобы div 1 строки не удалялся
+        }
+    }
 });
 
 
-
-
-
-
-// for (let i = 0; i < strsCode.children.length; i++) {
-//     strsCode.children[i].addEventListener('keydown', workWithStrCode);
-//     console.log(kek);
-// }
-
-// function workWithStrCode(event) {
-//     // if (event.key === 'Enter') {
-//     //     let div = document.createElement('div');
-//     //     div.id = strsCode.children.length;
-//     //     strsCode.appendChild(div);
-//     //     div.focus();
-//     //     div.addEventListener('keydown', workWithStrCode);
-//     // }
-//     console.log(event.key);
-//     if (event.key === '<' || event.key === '>') {
-//         console.log(event.key);
-//     }
-// }
