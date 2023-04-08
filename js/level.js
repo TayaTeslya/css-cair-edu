c0266993a7942bca00f9b7491a31272644428977
TayaTeslya <taya1teslya@gmail.com>
2023-04-09 00:04:42 +0300

    поле ввода кода (начало)

--- js/level.js
+++ js/level.js
@@ -2,3 +2,35 @@
 // localStorage.getItem('idLevel'); //получить - короче как сессионные переменные в пхп
 
 console.log(window.location);
+
+
+const strsCode = document.getElementById('strs-code');
+
+
+strsCode.addEventListener('keydown', (event) => {
+
+});
+
+
+
+
+
+
+// for (let i = 0; i < strsCode.children.length; i++) {
+//     strsCode.children[i].addEventListener('keydown', workWithStrCode);
+//     console.log(kek);
+// }
+
+// function workWithStrCode(event) {
+//     // if (event.key === 'Enter') {
+//     //     let div = document.createElement('div');
+//     //     div.id = strsCode.children.length;
+//     //     strsCode.appendChild(div);
+//     //     div.focus();
+//     //     div.addEventListener('keydown', workWithStrCode);
+//     // }
+//     console.log(event.key);
+//     if (event.key === '<' || event.key === '>') {
+//         console.log(event.key);
+//     }
+// }
