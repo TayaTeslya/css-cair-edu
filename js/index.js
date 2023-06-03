619050ac25c5c3a62e661cf831147af834cf1fda
TayaTeslya <taya1teslya@gmail.com>
2023-06-03 06:56:33 +0300

    api index.html full

--- js/index.js
+++ js/index.js
@@ -22,10 +22,34 @@ fetch(`http://localhost:3001/api/levels?idUser=${userInfo.id}`).then((res) => re
         favorite[i].addEventListener('click', (event) => { // событие клика по объекту избранное
             event.preventDefault(); //отключение стандартного поведение ссылки (родителя избранного)
             if (favorite[i].children[0].src.includes('/img/icons/not_favorite_icon.png')) { // изменение картинки избранного
-                favorite[i].children[0].src = favorite[i].children[0].src.replace('not_favorite', 'favorite');
+                console.log('post');
+                fetch(`http://localhost:3001/api/favorite`, { 
+                        method: 'POST', 
+                        headers: {'Content-Type' : 'application/json'}, 
+                        body: JSON.stringify({
+                            idUser: userInfo.id, 
+                            idLevel: event.target.dataset.idLevel
+                    })})
+                    .then((res) => res.json()).then((res) => {
+                    if (res) {
+                        favorite[i].children[0].src = favorite[i].children[0].src.replace('not_favorite', 'favorite');
+                    }
+                });
             }
             else {
-                favorite[i].children[0].src = favorite[i].children[0].src.replace('favorite', 'not_favorite');
+                console.log('delete');
+                fetch(`http://localhost:3001/api/favorite`, { 
+                        method: 'DELETE', 
+                        headers: {'Content-Type' : 'application/json'}, 
+                        body: JSON.stringify({
+                            idUser: userInfo.id, 
+                            idLevel: event.target.dataset.idLevel
+                    })})
+                    .then((res) => res.json()).then((res) => {
+                    if (res) {
+                        favorite[i].children[0].src = favorite[i].children[0].src.replace('favorite', 'not_favorite');
+                    }
+                });
             }
         });
     }
@@ -39,13 +63,12 @@ function setLevels(condition) {
     currentLevels = currentLevels.filter(condition);
     for (const level of currentLevels) { // цикл, проходящий по отфильтрованным по категориям уровням
         if (!userInfo.isStaff && level.isPublished) { // вывод доступных для учеников уровней
-
             levelContainer.innerHTML += 
             `<div class="d-flex py-0 px-0">
                 <a id="${level.id}" href="../pages/level.html#${level.id}" class="d-flex card justify-content-between flex-column gap-2">
                     <div class="card-img col-lg-12 col-md-12 col-sm-12 col-12 d-flex justify-content-center">
                         <img src="${level.thumbnail}" alt="">
-                        <div id="1" class="favorite anime-little">
+                        <div data-id-level='${level.id}' class="favorite anime-little">
                             <img src="${level.favorite ? '../img/icons/favorite_icon.png' : '../img/icons/not_favorite_icon.png'}" alt="">
                         </div>
                     </div>
